27d9904ae0048319709e2457af1dfc4ce76cfb74
William Chang <wilicw1346@gmail.com>
2021-01-27 19:07:44 +0800

    add jwt and move config into entities

--- server/entities.py
+++ server/entities.py
@@ -1,3 +1,22 @@
+from configparser import ConfigParser
+import jwt
+
+conf = ConfigParser()
+conf.read("config.ini", encoding="utf-8")
+
+
+class config:
+    db_config = conf["db"]
+    jwt_config = conf["jwt"]
+    db_protocol = db_config["protocol"]
+    db_user = db_config["user"]
+    db_pass = db_config["pass"]
+    db_host = db_config["host"]
+    db_port = db_config["port"]
+    db_name = db_config["db_name"]
+    secret = jwt_config["secret"]
+
+
 def remove_duplicates_preserving_order(seq):
     seen = set()
     seen_add = seen.add
@@ -10,3 +29,15 @@ def to_simple_obj_list(ll):
 
 def to_detail_obj_list(ll):
     return list(map(lambda x: x.to_detail(), ll))
+
+
+def generate_token(username):
+    return jwt.encode({"username": str(username)}, config.secret, algorithm="HS256")
+
+
+def decode_token(token):
+    try:
+        user = jwt.decode(token, config.secret, algorithms=["HS256"])["username"]
+    except:
+        user = -1
+    return user
--- server/main.py
+++ server/main.py
@@ -1,18 +1,15 @@
 #!.env/bin/python
 from flask import Flask, render_template
 from flask_restful import Api
-from configparser import ConfigParser
 import resources, model
+from entities import config
 from flask_cors import CORS
 
-conf = ConfigParser()
-conf.read("config.ini", encoding="utf-8")
-db_config = conf["db"]
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = f"{db_config['protocol']}://{db_config['user']}:{db_config['pass']}@{db_config['host']}:{db_config['port']}/{db_config['db_name']}"
+] = f"{config.db_protocol}://{config.db_user}:{config.db_pass}@{config.db_host}:{config.db_port}/{config.db_name}"
 
 model.db.init_app(app)
 CORS(app)

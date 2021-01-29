41a30ec1c64385daa45ce022b8c1b2244eae7106
William Chang <wilicw1346@gmail.com>
2021-01-29 09:48:17 +0800

    fix api url

--- ui/config.js
+++ ui/config.js
@@ -1,5 +1,7 @@
+import {publicPath} from "./vue.config"
 const config = {
-    api: process.env.NODE_ENV === 'production' ? '/api' : 'http://127.0.0.1:5000/api',
+    api: process.env.NODE_ENV == 'production' ? `${publicPath}api/` : 'http://127.0.0.1:5000/api',
     imgur_client_id: "e41ec52ccc51322"
 }
-export default { config }
+export { config }
+export default {  }
--- ui/src/api.js
+++ ui/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import config from '../config'
+import {config} from '../config'
 
 const client = axios.create({
   baseURL: config.api,

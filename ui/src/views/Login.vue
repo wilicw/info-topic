f4b7b457e0fc65d2f65f33f5ac638259d6b34b6b
William Chang <wilicw1346@gmail.com>
2021-01-30 09:19:38 +0800

    v-form use submit event

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -3,32 +3,43 @@
     <v-row justify="center" class="mt-5">
       <v-col cols="12" md="4">
         <v-card :flat="$vuetify.breakpoint.mobile" class="pa-5">
-          <v-card-title class="font-weight-bold">登入</v-card-title>
-          <v-alert
-            :value="err"
-            color="pink"
-            dark
-            border="top"
-            icon="mdi-close"
-            transition="scale-transition"
-          >
-            帳號或密碼錯誤
-          </v-alert>
-          <v-card-text>
-            帳號請輸入 畢業級別+班級+座號 ，例如108年畢業甲班55號，請輸入108a55
-            <br />
-            <br />
-            <v-text-field
-              v-model="account"
-              label="帳號"
-              required
-            ></v-text-field>
-            <v-text-field v-model="pass" label="密碼" required></v-text-field>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" @click="login">送出</v-btn>
-          </v-card-actions>
+          <v-form @submit.prevent="login">
+            <v-card-title class="font-weight-bold">登入</v-card-title>
+            <v-alert
+              :value="err"
+              color="pink"
+              dark
+              border="right"
+              icon="mdi-close"
+              transition="scale-transition"
+            >
+              帳號或密碼錯誤
+            </v-alert>
+            <v-card-text>
+              <p class="text--secondary">
+                帳號請輸入 畢業級別+班級+座號
+                ，例如108年畢業甲班55號，請輸入108a55
+              </p>
+              <v-text-field
+                v-model="account"
+                label="帳號"
+                required
+                :rules="rules"
+                class="mt-8"
+              ></v-text-field>
+              <v-text-field
+                v-model="pass"
+                label="密碼"
+                required
+                type="password"
+                :rules="rules"
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" type="submit">送出</v-btn>
+            </v-card-actions>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
@@ -44,7 +55,11 @@ export default {
     pass: "",
     account: "",
     err: null,
+    rules: [(value) => !!value || "此欄位不可空白！"],
   }),
+  created() {
+    if (api.is_login()) this.$router.push("/menu");
+  },
   methods: {
     async login() {
       const username = this.account,
--- ui/src/views/Search.vue
+++ ui/src/views/Search.vue
@@ -4,34 +4,21 @@
       <v-col cols="12" md="6">
         <v-card class="rounded-0 pa-5" :flat="$vuetify.breakpoint.mobile">
           <v-card-title class="font-weight-bold">搜尋專題</v-card-title>
-          <v-card-text>
-            <br />
-            <v-text-field
-              v-model="keywords"
-              label="關鍵字"
-              required
-            ></v-text-field>
-            <v-row>
-              <v-col
-                cols="6"
-                sm="3"
-                v-for="c in search_options"
-                :key="c.value"
-                class="my-0"
-              >
-                <v-checkbox
-                  v-model="columns"
-                  :label="c.text"
-                  :value="c.value"
-                  class="my-0"
-                ></v-checkbox
-              ></v-col>
-            </v-row>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" @click="search">送出</v-btn>
-          </v-card-actions>
+          <v-form @submit.prevent="search">
+            <v-card-text>
+              <v-text-field
+                v-model="keywords"
+                label="關鍵字"
+                prepend-icon="mdi-magnify"
+                pre-icon="required"
+                class="mt-5"
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" type="submit">送出</v-btn>
+            </v-card-actions>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
@@ -43,30 +30,12 @@ export default {
   name: "Search",
   data: () => ({
     keywords: "",
-    columns: [],
-    search_options: [
-      {
-        text: "專題名稱",
-        value: "name",
-      },
-      {
-        text: "學生",
-        value: "student",
-      },
-      {
-        text: "指導老師",
-        value: "teacher",
-      },
-      {
-        text: "摘要",
-        value: "abstract",
-      },
-    ],
   }),
   methods: {
     search() {
       const keywords = this.keywords;
-      this.$router.push(`/search/${keywords}`);
+      if (keywords.length != 0) this.$router.push(`/search/${keywords}`);
+      return;
     },
   },
 };

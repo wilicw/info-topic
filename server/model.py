d30c485ba126a04888a8c4455371415deb4433a6
William Chang <wilicw1346@gmail.com>
2021-02-21 13:11:15 +0800

    SQL use pool_pre_ping

--- server/model.py
+++ server/model.py
@@ -1,8 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
-from entities import cjk_layout, config
+from entities import cjk_layout
+from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+
+
+class SQLAlchemy(_BaseSQLAlchemy):
+    def apply_pool_defaults(self, app, options):
+        super(SQLAlchemy, self).apply_pool_defaults(app, options)
+        options['pool_pre_ping'] = True
+
 
 db = SQLAlchemy()
-
 
 class Admin(db.Model):
     __tablename__ = "admin"

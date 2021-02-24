f4278c241b358ba36568723c00e219e7b664eae0
William Chang <wilicw1346@gmail.com>
2021-02-24 15:37:56 +0800

    Improve ui

--- ui/src/views/Public/Reference.vue
+++ ui/src/views/Public/Reference.vue
@@ -1,7 +1,12 @@
 <template>
   <v-row justify="center" class="mt-3">
     <v-col cols="12" lg="8" sm="10">
-      <v-card class="rounded-0 mt-5 pa-3"> <div v-html="ref"></div> </v-card>
+      <v-card class="rounded-0 mt-5 pa-3">
+        <p class="display-1 font-weight-medium mt-3 ml-3">
+          <v-icon class="mb-1" color="primary">mdi-pound</v-icon> 參考資料
+        </p>
+        <div class="mt-10" v-html="ref"></div>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -16,8 +21,18 @@ export default {
     ref: "",
   }),
   async created() {
-    const res = await api.get_reference();
-    this.ref = res.data;
+    try {
+      const res = await api.get_reference();
+      this.ref = res.data;
+    } catch (error) {
+      this.ref = ``;
+    }
   },
 };
 </script>
+
+<style scoped>
+blockquote {
+  margin: 2em;
+}
+</style>

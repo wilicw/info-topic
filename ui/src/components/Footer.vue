e6e5ccfa7b2e786a71af18ba5873ed903b292079
William Chang <wilicw1346@gmail.com>
2021-02-25 17:03:09 +0800

    Edit footer

--- ui/src/components/Footer.vue
+++ ui/src/components/Footer.vue
@@ -1,16 +1,6 @@
 <template>
   <v-card flat class="text-center ma-10" style="background-color: transparent">
     <v-card-text>
-      臺北市立大安高工資訊科
-      <br />
-      專題製作成果網站
-      <br />
-      校址：(106317)臺北市大安區復興南路2段52號
-      <br />
-      No. 52, Sec. 2, Fusing S. Rd., Taipei City, Taiwan 10664, R.O.C.
-      <br />
-      TEL: (02)27091630 FAX: (02) 27090635
-      <br />
       <br />
       Copyright © {{ new Date().getFullYear() }} —
       <strong>大安高工資訊科</strong>

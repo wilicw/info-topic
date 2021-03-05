325afffcc621df56fafd7b247046bf923a6fc653
William Chang <wilicw1346@gmail.com>
2021-03-05 15:22:05 +0800

    Add scipy

--- ui/src/views/Public/Rank_topics.vue
+++ ui/src/views/Public/Rank_topics.vue
@@ -40,7 +40,8 @@
 <script>
 import _ from "lodash";
 import Loading from "@/components/Loading-4.vue";
-import Multi_Topics from "@/components/Multi_Topics.vue";
+// import Multi_Topics from "@/components/Multi_Topics.vue";
+import Multi_Topics from "@/components/List_topics.vue"
 import api from "@/api";
 import { config } from "@/../config";
 

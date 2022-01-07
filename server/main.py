bd0a773b582d2ac7ac03534eaf7a5058d089fd0f
William Chang <wilicw1346@gmail.com>
2022-01-07 11:46:36 +0800

    Change address to 0.0.0.0

--- server/main.py
+++ server/main.py
@@ -35,4 +35,4 @@ app.register_blueprint(teachers.api_bp)
 app.register_blueprint(years.api_bp)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=True)
+    app.run(host="0.0.0.0", debug=True)

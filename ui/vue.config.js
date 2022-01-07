a1c77aae6c8cdf62ffc09d7fe902874a4cc6af25
William Chang <wilicw1346@gmail.com>
2022-01-07 18:06:14 +0800

    fix publicPath

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  // publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   outputDir: '/tmp/dist',
 }

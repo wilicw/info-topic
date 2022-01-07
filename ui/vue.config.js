96140fd674cf5bfe2141249b82211c7a60752bf6
William Chang <wilicw1346@gmail.com>
2022-01-07 17:45:01 +0800

    Test server side render

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/topic/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: '/',
   outputDir: '/tmp/dist',
 }
